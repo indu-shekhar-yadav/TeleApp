6d5bb3da7af7fed8dd39967a0e48cf01deb2ae6d
Indu Shekhar Yadav <shekharindu21@gmail.com>
2023-09-20 22:46:52 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+const projectID = '63b57e98-918c-4e6d-9ba5-019ed33dfc2d';
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
+export default App;
